b60f6a463b9915b4824eb92e4c50562ad03040f6
animesh <animesh@ebctechnologies.com>
2017-04-21 02:25:57 +0530

    adding scala learning

---
+++ src/main/scala/FirstScala.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by animesh on 4/8/17.
+  */
+object FirstScala {
+
+  def main(args: Array[String]): Unit = {
+    println("hi  hi");
+  }
+
+}
---
+++ src/main/scala/TryFirst.sc
@@ -0,0 +1,10 @@
+object TryNLearn{
+
+  print("hi world hi how r u !")
+
+  val num: Int = 4
+  val num1: Int = 5
+
+  print(num + num1)
+
+}
